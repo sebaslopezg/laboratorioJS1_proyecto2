157b67aae7bfc5993287ebb3f8d8640801708034
sebas <sebaslg96@gmail.com>
2024-08-13 06:57:37 -0500

    agregado las imagenes

--- assets/js/data.js
+++ assets/js/data.js
@@ -7,87 +7,87 @@ let inventario = {
         },
         1:{
             nombre:'JA guayana',
-            img:'url/ejemplo',
+            img:'hawayana.jpg',
             precio:1234
         },
         2:{
             nombre:'Champions ñones',
-            img:'url/ejemplo',
+            img:'champions.png',
             precio:1234
         },
         3:{
             nombre:'Carne con Pasta',
-            img:'url/ejemplo',
+            img:'carne.jpg',
             precio:1234
         },
         4:{
             nombre:'Pizza especial de sancocho de gallina',
-            img:'url/ejemplo',
+            img:'sancocho.png',
             precio:1234
         },
         5:{
             nombre:'Pizza de arequipe',
-            img:'url/ejemplo',
+            img:'arequipe.png',
             precio:1234
         }
     },
     extra:{
         0:{
-            nombre:'ejemplo',
-            img:'url/ejemplo',
-            precio:1234
+            nombre:'Tajin',
+            img:'tajin.png',
+            precio:2300
         },
         1:{
-            nombre:'ejemplo',
-            img:'url/ejemplo',
-            precio:1234
+            nombre:'Salsal BBQ',
+            img:'bbq.webp',
+            precio:12400
         },
         2:{
-            nombre:'ejemplo',
-            img:'url/ejemplo',
-            precio:1234
+            nombre:'Tocineta',
+            img:'bacon.png',
+            precio:2000
         },
         3:{
-            nombre:'ejemplo',
-            img:'url/ejemplo',
-            precio:1234
+            nombre:'Acompañante chedar',
+            img:'chedar.jpg',
+            precio:21000
         },
         4:{
-            nombre:'ejemplo',
-            img:'url/ejemplo',
-            precio:1234
+            nombre:'Papita',
+            img:'papa.jpg',
+            precio:60000
         },
         5:{
-            nombre:'ejemplo',
-            img:'url/ejemplo',
-            precio:1234
+            nombre:'Platano con queso',
+            img:'platano.jpg',
+            precio:2000
         }
     },
     bebidas:{
         0:{
-            nombre:'ejemplo',
-            img:'url/ejemplo',
-            precio:1234
+            nombre:'Moka-Kola',
+            img:'mokacola.jpg',
+            precio:4000
         },
         1:{
-            nombre:'ejemplo',
-            img:'url/ejemplo',
-            precio:1234
+            nombre:'Cerveza liquida',
+            img:'cerveza.jpg',
+            precio:6000
         },
         2:{
-            nombre:'ejemplo',
-            img:'url/ejemplo',
-            precio:1234
+            nombre:'Chocolate Caliente',
+            img:'chocolate.webp',
+            precio:5000
         },
         3:{
-            nombre:'ejemplo',
-            img:'url/ejemplo',
-            precio:1234
+            nombre:'Jabon Liquido',
+            img:'jabon.png',
+            precio:6000
         },
         4:{
-            nombre:'ejemplo',
-            img:'url/ejemplo',
-            precio:1234
+            nombre:'Refrescante Bebida a base de petroleo',
+            img:'petroleo.jpg',
+            precio:200000
         }
     }
 }
